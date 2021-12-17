cd6653c1440c364fcdf698580b9e37bba857fa42
Matthew Polizzotti <MPolizzotti-consultant@Scholastic.com>
2021-12-17 08:17:32 -0700

    feat(intersect): add solution

--- algorithms/intersect/intersect.mjs
+++ algorithms/intersect/intersect.mjs
@@ -1,23 +1,10 @@
 const intersect = (nums1, nums2) => {
-    // 1] Iterate over larger array.
-    // 2] Place values in an object hash.
-    // 3] Test seconadry array against object hash.
-
-    // if (nums1.length >= nums2.length) {
-    //   lArr = nums1;
-    //   sArr = nums2;
-    // } else {
-    //   lArr = nums2;
-    //   sArr = nums1;
-    // }
-
     const lArr = (nums1.length >= nums2.length) ? nums1 : nums2;
     const sArr = (nums1.length >= nums2.length) ? nums2 : nums1;
     const result = [];
 
     for (let i = 0; i <= sArr.length - 1; i++) {
       const indexOf = lArr.indexOf(sArr[i]);
-      console.log('indexOf: ', indexOf);
 
       if (indexOf !== -1) {
         result.push(sArr[i]);
@@ -25,15 +12,7 @@ const intersect = (nums1, nums2) => {
       }
     }
 
-    console.log('result: ', result);
     return result;
 };
-
-
-
-const nums1 = [1,2];
-const nums2 = [1,1];
-const expected = [1];
-intersect(nums1, nums2);
 
 export default intersect;
