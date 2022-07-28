501d860efde70af14ede3035b9fe3cd5d1bbe915
Matthew Polizzotti <MPolizzotti-consultant@Scholastic.com>
2022-07-27 21:29:27 -0700

    feat(containsDuplicate): add solution

--- algorithms/contains.duplicate/contains.duplicate.mjs
+++
@@ -1,7 +0,0 @@
-const containsDuplicate = function(nums) {
-  // Define a Set, which only allows unique values
-  // and compare it with the length of the nums array.
-  return new Set(nums).size < nums.length;
-};
-
-export default containsDuplicate;
