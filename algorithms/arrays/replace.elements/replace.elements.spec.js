ba76c65431a771efeaca1e95b7a2e4c2d644fd90
Matthew Polizzotti <MPolizzotti-consultant@Scholastic.com>
2022-07-29 15:49:58 -0700

    test(replaceElements): resolve failing test

--- algorithms/arrays/replace.elements/replace.elements.spec.js
+++ algorithms/arrays/replace.elements/replace.elements.spec.js
@@ -15,11 +15,11 @@ describe('replaceElements', () => {
 describe('replaceElementsOptimized', () => {
   it('should return array with greatest element on the right side', () => {
     const arr = [17,18,5,4,6,1];
-    expect(replaceElements(arr)).toEqual([18,6,6,6,1,-1]);
+    expect(replaceElementsOptimized(arr)).toEqual([18,6,6,6,1,-1]);
   });
 
   it('should return an array with a -1 at the first index', () => {
     const arr = [400];
-    expect(replareplaceElementsOptimizedceElements(arr)).toEqual([-1]);
+    expect(replaceElementsOptimized(arr)).toEqual([-1]);
   });
 });
