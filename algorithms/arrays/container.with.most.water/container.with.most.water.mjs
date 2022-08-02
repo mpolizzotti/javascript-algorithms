8fdead38daea0329214dc499885a4184b954e3ed
Matthew Polizzotti <MPolizzotti-consultant@Scholastic.com>
2022-08-02 16:27:42 -0700

    feat(containerOfWater): add solution

---
+++ algorithms/arrays/container.with.most.water/container.with.most.water.mjs
@@ -0,0 +1,26 @@
+const maxArea = (height) => {
+  let l = 0;
+  let r = height.length - 1; // 8
+  let maxArea = 0;
+
+  while (l < r) {
+    let minHeight = Math.min(height[l], height[r]);
+    let area =  minHeight * (r - l);
+    maxArea = Math.max(maxArea, area);
+
+    if (height[r] > height[l]) {
+      l = l + 1;
+    } else if (height[r] < height[l]) {
+      r = r - 1;
+    } else {
+      l = l + 1;
+    }
+  }
+
+  return maxArea;
+};
+
+const height = [1,8,6,2,5,4,8,3,7];
+console.log(maxArea(height));
+
+export default maxArea;
